413a69cafeed619c1ff62c2af1a278f47a61abc3
prateek taluja <prateek.taluja@whizkidzmedia.com>
2023-09-28 15:31:07 +0530

    notification testing

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+Notification.requestPermission(function (result) {
+    if (result === 'granted') {
+        navigator.serviceWorker.ready.then(function (registration) {
+            registration.showNotification('Notification with ServiceWorker');
+        });
+    }
+});
