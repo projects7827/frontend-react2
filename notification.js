ba3d1184ec23ae1ea410bb4c66b2e555bf3b3253
prateek taluja <prateek.taluja@whizkidzmedia.com>
2023-10-02 14:49:39 +0530

    analytics testing

--- notification.js
+++ notification.js
@@ -1,6 +1,18 @@
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging.js";
-import { app } from "./firebase"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDkeQhIjKjXl6ruJwZ3lOG0LwSa5-T9qLs",
+    authDomain: "prateek-5bd13.firebaseapp.com",
+    projectId: "prateek-5bd13",
+    storageBucket: "prateek-5bd13.appspot.com",
+    messagingSenderId: "550542718433",
+    appId: "1:550542718433:web:1d980152172f1ba987cea0",
+    measurementId: "G-4CHHHCPN48"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export { app }
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-import { app } from "./firebase";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDkeQhIjKjXl6ruJwZ3lOG0LwSa5-T9qLs",
+    authDomain: "prateek-5bd13.firebaseapp.com",
+    projectId: "prateek-5bd13",
+    storageBucket: "prateek-5bd13.appspot.com",
+    messagingSenderId: "550542718433",
+    appId: "1:550542718433:web:1d980152172f1ba987cea0",
+    measurementId: "G-4CHHHCPN48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export { app }
+
 let analytics = getAnalytics(app)
 console.log(analytics)
-function redirectSection(page) {s
+function redirectSection(page) {
+    s
     let formContainer = document.querySelector(".formContainer")
     let detailsContainer = document.querySelector(".detailsContainer")
     let detailRedirection = document.querySelector(".redirectToDetails")
